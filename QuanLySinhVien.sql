48dd4f600227b0b810e29e619315c2ae415428d9
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-07-19 14:44:30 +0700

    Add files via upload

--- QuanLySinhVien.sql
+++ QuanLySinhVien.sql
@@ -33,12 +33,16 @@ foreign key (subid) references subject (subid),
 foreign key (studentid) references student (studentid)
 );
 
+
+
 INSERT INTO Class
 VALUES (1, 'A1', '2008-12-20', 1);
 INSERT INTO Class
 VALUES (2, 'A2', '2008-12-22', 1);
 INSERT INTO Class
 VALUES (3, 'B3', current_date, 0);
+INSERT INTO Class
+VALUES (4, 'A4', '2012-11-22', 1);
 
 INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
@@ -81,6 +85,22 @@ FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.Su
 WHERE Sub.SubName = 'CF';
 
 SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
-FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId       
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId;
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+select * from student where studentName like 'H%';
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+select * from class where startDate like '%-12-%';
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+select * from subject where creadit between 3 and 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update student set classID = '2' where (studentID = '1');
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select studentName , SubName , Mark from ((student inner join subject on student.studentID = subject.subid )
+inner join mark on subject.subid = mark.markid )  order by mark desc;
 
 
