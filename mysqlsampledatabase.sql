b40620c5cb953804d9b595f1e92b34c0cbd5f9aa
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-07-21 16:48:30 +0700

    Add files via upload

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -7942,4 +7942,19 @@ EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers;
+END //
+DELIMITER ;
+
+call findAllCustomers();
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+SELECT * FROM customers where customerNumber = 175;
+END //
 
